598ee06ef661624db24cf236588736f77a671352
aRustyDev <36318507+aRustyDev@users.noreply.github.com>
2025-09-27 03:14:05 -0400

    feat(langium): version 1 of the tide language syntax

--- langium/Neptune/packages/language/src/tide-validator.ts
+++ langium/Neptune/packages/language/src/tide-validator.ts
@@ -1,5 +1,5 @@
 import type { ValidationAcceptor, ValidationChecks } from 'langium';
-import type { TideAstType, Person } from './generated/ast.js';
+import type { TideAstType, Model } from './generated/ast.js';
 import type { TideServices } from './tide-module.js';
 
 /**
@@ -9,23 +9,23 @@ export function registerValidationChecks(services: TideServices) {
     const registry = services.validation.ValidationRegistry;
     const validator = services.validation.TideValidator;
     const checks: ValidationChecks<TideAstType> = {
-        Person: validator.checkPersonStartsWithCapital
+        Model: validator.checkPersonStartsWithCapital
     };
     registry.register(checks, validator);
 }
 
-/**
- * Implementation of custom validations.
- */
-export class TideValidator {
+// /**
+//  * Implementation of custom validations.
+//  */
+// export class TideValidator {
 
-    checkPersonStartsWithCapital(person: Person, accept: ValidationAcceptor): void {
-        if (person.name) {
-            const firstChar = person.name.substring(0, 1);
-            if (firstChar.toUpperCase() !== firstChar) {
-                accept('warning', 'Person name should start with a capital.', { node: person, property: 'name' });
-            }
-        }
-    }
+//     checkPersonStartsWithCapital(model: Model, accept: ValidationAcceptor): void {
+//         if (model.tide) {
+//             const firstChar = model.var.substring(0, 1);
+//             if (firstChar.toUpperCase() !== firstChar) {
+//                 accept('warning', 'Person name should start with a capital.', { node: model, property: 'diagrams' });
+//             }
+//         }
+//     }
 
-}
+// }
---
+++ langium/justfile
@@ -0,0 +1,19 @@
+# Output JSONSchema to .data/
+schema:
+    echo "Todo"
+
+# Convert Langium Grammar to Tree-sitter Grammar
+grammar:
+    echo "Todo"
+
+# Convert Langium Language Validation to Zed-Extension LSP Validation
+validations:
+    echo "Todo"
+
+# Convert Langium AST to Tree-sitter AST
+ast:
+    echo "Todo"
+
+# Convert Langium Tests to Tree-sitter Tests
+tests:
+    echo "Todo"
